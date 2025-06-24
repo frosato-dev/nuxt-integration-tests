aac491c90a134eb14752d97e5150a6392ed98119
François Rosato <rosato.francois@gmail.com>
2025-06-24 14:14:12 +0200

    base project

---
+++ pages/demo-1.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Demo 1</h1>
+    <NuxtLink to="/">Retour à l'accueil</NuxtLink>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const { data } = await useFetch<{ fizz: "buzz" }>(
+  "https://mockanapi.com/s/6773ca761e6f1a48a311752a/test2",
+  {
+    responseType: "json",
+  }
+);
+</script>
+
+<template>
+  <div>
+    <h1>Homepage</h1>
+    <div v-if="data" data-testid="api">{{ data.fizz }}</div>
+    <NuxtLink to="/demo-1">Aller à Demo 1</NuxtLink>
+  </div>
+</template>
